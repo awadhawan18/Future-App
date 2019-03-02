8ff38153e9d486dc5e57fc891817b29d0d0914ef
manik <maniksejwal@gmail.com>
2019-03-03 03:09:03 +0530

    moved from gradient to bars

--- app/src/main/java/com/futureapp/PlantAdapter.java
+++ app/src/main/java/com/futureapp/PlantAdapter.java
@@ -3,11 +3,9 @@ package com.futureapp;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
 import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.design.card.MaterialCardView;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -44,17 +42,21 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.MyViewHolder
         holder.renewable.setText(String.valueOf(plant.renewable));
         holder.nonrenewable.setText(String.valueOf(plant.nonrenewable));
 
-        Drawable drawable = new DrawableBuilder()
-                .rectangle()
-                .gradient()
-                .gradientType(GradientDrawable.)
-                .linearGradient()
-                .startColor(0xFF00FFFF)
-                .endColor(0xFFFF0000)
-                .ripple()
-                .build();
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0,
+                200, (float) plant.renewable/plant.total);
+        holder.image_renewable.setLayoutParams(params);
+        holder.image_renewable.setImageResource(R.drawable.gradient_renewable);
 
-        holder.plantView.setBackground(drawable);
+        params = new LinearLayout.LayoutParams(0,
+                200, (float) plant.nonrenewable/plant.total);
+        holder.image_nonrenewable.setLayoutParams(params);
+        holder.image_nonrenewable.setImageResource(R.drawable.gradient_nonrenewable);
+
+//        android:type="linear"
+//        android:startColor="@color/green"
+//        android:endColor="@color/red"
+//        android:centerColor="@color/colorPrimaryDark"
+//        android:centerX="0.5"
     }
 
     @Override
@@ -65,8 +67,9 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.MyViewHolder
     static class MyViewHolder extends RecyclerView.ViewHolder {
         private TextView name;
         TextView total, renewable, nonrenewable;
-        LinearLayout plantView;
+        LinearLayout plantView, background_layout;
         MaterialCardView plantCard;
+        ImageView image_renewable, image_nonrenewable;
 
         MyViewHolder(View itemView) {
             super(itemView);
@@ -76,6 +79,9 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.MyViewHolder
             nonrenewable = itemView.findViewById(R.id.plant_nonrenewable);
             plantView = itemView.findViewById(R.id.plant_view);
             plantCard = itemView.findViewById(R.id.plant_card);
+            image_renewable = itemView.findViewById(R.id.image_renewable);
+            image_nonrenewable = itemView.findViewById(R.id.image_nonrenewable);
+            background_layout = itemView.findViewById(R.id.background_layout);
         }
     }
 }
